a3f1f5cccb19103a9915a01e09377efde4810182
Mehmet Yatkı <26697004+yatki@users.noreply.github.com>
2022-07-06 13:48:02 +0200

    Add useInterval

---
+++ use-interval.js
@@ -0,0 +1,23 @@
+// From: https://overreacted.io/making-setinterval-declarative-with-react-hooks/#just-show-me-the-code
+
+import React, { useState, useEffect, useRef } from 'react';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
